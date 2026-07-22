9d96e9a9bc436f347f50ecc68499cdbe7049d05f
Gabriella Rodrigues Pimenta <gabsrpimentavdg@gmail.com>
2026-07-22 16:50:26 +0100

    feat: Correcao do exercicio 7

--- src/exercicio7/Main.java
+++ src/exercicio7/Main.java
@@ -7,20 +7,24 @@ public class Main {
         curso1.setNome("IEFP");
         curso1.setDuracaoEmHoras(300);
 
+        Curso curso2 = new Curso();
+        curso2.setNome("ISLA");
+        curso2.setDuracaoEmHoras(300);
+
         CursoOnline cursoOnline1 = new CursoOnline();
         cursoOnline1.setNome("IEFP Online");
         cursoOnline1.setDuracaoEmHoras(250);
         cursoOnline1.setLinkAcesso("https://www.iefp.pt");
 
-        Curso curso2 = new CursoOnline();
-        curso2.setNome("ISLA");
-        curso2.setDuracaoEmHoras(200);
-        ((CursoOnline) curso2).setLinkAcesso("https://www.isla.pt");
+        CursoOnline cursoOnline2 = new CursoOnline();
+        cursoOnline2.setNome("ISLA");
+        cursoOnline2.setDuracaoEmHoras(250);
+        cursoOnline2.setLinkAcesso("https://www.isla.pt");
 
         System.out.println(curso1.exibirResumo());
+        System.out.println(curso2.exibirResumo());
 
         System.out.println(cursoOnline1.exibirResumo());
-
-        System.out.println(curso2.exibirResumo());
+        System.out.println(cursoOnline2.exibirResumo());
     }
 }
